b183f64cdd67200606c8d2ca2c860bf96452c93f
Marcoliiin <gabrielmarcolin10@gmail.com>
2024-03-16 12:14:28 -0300

    teste

--- src/main/java/Mercadinho/learning/Clients.java
+++ src/main/java/Mercadinho/learning/Clients.java
@@ -17,7 +17,6 @@ public class Clients {
             System.out.println("Cliente cadastrado com sucesso.");
         } catch (Exception e) {
             System.err.println("Erro ao cadastrar o cliente: " + e.getMessage());
-            System.out.println("Teste");
         }
     }
 }
